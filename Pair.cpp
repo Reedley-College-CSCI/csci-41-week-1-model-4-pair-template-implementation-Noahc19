e79ed3ad948a55df6f0ffe403dba112f4dceb92f
Noah Conchas <Conchasnoah8@gmail.com>
2025-08-20 11:55:47 -0700

    Completed getters and setters.

--- Pair.cpp
+++ Pair.cpp
@@ -8,3 +8,23 @@ Pair<T>::Pair(T f, T s) {
 	first = f;
 	second = s;
 }
+
+template <typename T>
+T Pair<T>::getFirst() const {
+	return first;
+}
+
+template <typename T>
+T Pair<T>::getSecond() const {
+	return second;
+}
+
+template <typename T>
+void Pair<T>::setFirst(T f) {
+	first = f;
+}
+
+template <typename T>
+void Pair<T>::setSecond(T s) {
+	second = s;
+}
