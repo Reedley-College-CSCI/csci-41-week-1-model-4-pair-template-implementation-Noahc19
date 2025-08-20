42c214e84955f02ea626ea59322cde88ef6f93e8
Noah Conchas <Conchasnoah8@gmail.com>
2025-08-20 12:05:05 -0700

    Final cleanup and comments.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 // Noah Conchas
-#include "Pair.h"
-#include "Pair.cpp"
+#include "Pair.h" // file with class
+#include "Pair.cpp" // file with member functions
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
 	Pair<int> p1(1, 2);
-	Pair<string> p2("apple", "banna");
+	Pair<string> p2("apple", "banana");
 	Pair<char> p3('a', 'b');
+	
+	cout << "Pair 1: " << p1.getFirst() << ", " << p1.getSecond() << endl; // Output: Pair 1: 1, 2
 
-	cout << "Pair 1: " << p1.getFirst() << ", " << p1.getSecond() << endl;
+	cout << "Pair 2: " << p2.getFirst() << ", " << p2.getSecond() << endl; // Output: Pair 2: apple, banana
 
-	cout << "Pair 2: " << p2.getFirst() << ", " << p2.getSecond() << endl;
-
-	cout << "Pair 3: " << p3.getFirst() << ", " << p3.getSecond() << endl;
+	cout << "Pair 3: " << p3.getFirst() << ", " << p3.getSecond() << endl; // Output: Pair 3: a, b
 
 
     return 0;
